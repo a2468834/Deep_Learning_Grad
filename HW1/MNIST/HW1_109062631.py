3c1707c82b91692bcb14a1c6da30112814e491d5
a2468834 <a2468834@gmail.com>
2020-11-05 01:58:07 +0800

    Make some modifications in HW1.

--- HW1/MNIST/HW1_109062631.py
+++ HW1/MNIST/HW1_109062631.py
@@ -11,20 +11,22 @@
 #      Hardware: Intel i7-10510U, 16GB DDR4 non-ECC ram, and no discrete GPU
 import math
 import matplotlib.pyplot as pyplot
+from   multiprocessing import Pool
 import numpy
 import os
 import pathlib
+import pickle
 import random
 import re
-from   scipy.special import *
+from   scipy.special import softmax
 import struct
 import sys
 import time
 import warnings
 
 
-# Define some constants
 class CONST:
+    # Define some constants
     row_num    = lambda : 28                                # height of a image in pixels
     col_num    = lambda : 28                                # width of a image in pixels
     input_dim  = lambda : CONST.row_num() * CONST.col_num()
@@ -33,51 +35,106 @@ class CONST:
     v_t_ratio  = lambda : 0.3                               # validation_training_ration
 
 
-# Define some basic functions
 class FUNC:
-    unitStep = lambda x : (x > 0.0).astype(numpy.int)                    # 1st Derivative of reLU w.r.t. 'x'
-    reLU     = lambda x : x * (x > 0.0)
+    # Define some basic functions
+    dReLU = lambda x : (x > 0.0).astype(numpy.float)        # 1st Derivative of reLU w.r.t. 'x', i.e., unit step function
+    reLU  = lambda x : x * (x > 0.0)
     
     #C-E Loss = $-\sum{\mathrm{label}_i{\ }log_{e}(\mathrm{predict}_i)}$, where $i{\in}$ vector's index set
-    def crossEntropy(predict_vector, truth_vector):
-        reshape_predict_v, truth_label = predict_vector.reshape(-1), numpy.argmax(truth_vector, axis=0)
-        if reshape_predict_v[truth_label] == 0.0:
-            return (-1.0) * numpy.log(0.0001)
-        else:
-            return (-1.0) * numpy.log(reshape_predict_v[truth_label])
-    
-    def binaryIndicator(vector, relation, comp_value):
-        if relation not in [">", "<", "="]:
-            print("Relation used in binary indicator must be '>', '<', or '='.")
+    def crossEntropy(predict_vec, truth_vec):
+        if predict_vec.shape != truth_vec.shape:
+            print("Wrong dimension of predict vector and truth vector in cross entropy.")
             exit()
         else:
-            if relation == ">": return (vector > 0.0).astype(numpy.int)
-            elif relation == "<": return (vector < 0.0).astype(numpy.int)
-            else: return (vector == 0.0).astype(numpy.int)
+            # Add tiny amount 1e-8 to avoid put zero into numpy.log()
+            return (-1.0) * numpy.sum(truth_vec * numpy.log(predict_vec.T + 1e-8))
 
 
-# 'network_struct' tells us that the # of nodes at each layer, e.g., [input_dim, 1st_hd_layer, ..., output_dim]
-# 'W' : list of weight matrices, which represents each layer's W. Note that W[0] is W1.
-# 'b' : list of bias vectors, which represents each layer's b. Note that b[0] is b1.
 class MODEL:
+    # 'network_struct' tells us that the # of nodes at each layer, e.g., [input_dim, 1st_hd_layer, ..., output_dim]
+    # 'W' : a list of weight matrices. E.g., W[0] is the weight matrix connected input layer and 1st hidden layer.
     def __init__(self, network_struct, learning_rate, bias=None):
         # Local variables
         self.network_struct = network_struct
         self.learning_rate = learning_rate 
-        self.bias = bias
         self.W = initWeights(network_struct) # Initialize weight metrices
 
 
+
+########## Header of all functions #############################################
 def initWeights(network_struct):
-    # Initialize parameters within all weight matrices 'weignts' and bias vectors 'bias'.
+    # Initialize parameters within all weight matrices 'W'.
+    return NotImplemented
+def loadData(image_file_path, label_file_path):
+    # Every COLUMN (NOT row) in 'x_part' is a training/testing instance.
+    # Turn labels in y_part into one-hot vectors.
+    return NotImplemented
+def readImageLabel(file_path, file_type):
+    ## See below ##
+    return NotImplemented
+def makeDataBatches(batch_size, data_x_part, data_y_part):
+    # Return a list of list of batches [batch_0, batch_1, batch_2,...], 
+    # where batch_i = [tuple(one_instance_x, one_instance_y), tuple_1,...]
+    return NotImplemented
+def oneHotEncoding(data_y_part):
+    ## See below ##
+    return NotImplemented
+def trainModel(num_epochs, model, train_x_part, train_y_part, valid_x_part, valid_y_part):
+    ## See below ##
+    return NotImplemented
+def updateModelWeight(update_method, model, nabla_W, batch_size=0):
+    ## See below ##
+    return NotImplemented
+def forwardPropagate(model, data_x_part):
+    # 'h' is a list of dicts which stores intermediate values (i.e., pre_activation_value and
+    #  post_activation_value) at hidden layers and output layer.
+    # NOTE1: h[0]['pre_act'] = h[0]['post_act'] = the x part of input data
+    # NOTE2: 'pre_act' is 'Z' matrix i.e., Z = W[prev] * A[prev], 'pre_act' = 'A' = Activation(Z)
+    return NotImplemented
+def backwardPropagate(model, Intermediates, truth_y):
+    # Return 'nabla_W'
+    # 'nabla_W' is a layer-by-layer python-list of numpy arrays, similar to 'model.W'
+    return NotImplemented
+def actFunc(pre_activation_value, layer_type):
+    ## See below ##
+    return NotImplemented
+def deActFunc(layer_type, z_vector=None, predict_y=None, truth_y=None):
+    ## See below ##
+    return NotImplemented
+def calcPreActZ(weight, data_x_part):
+    ## See below ##
+    return NotImplemented
+def inference(model, data_x_part):
+    ## See below ##
+    return NotImplemented
+def isNotEqual(predict_y, truth_y):
+    ## See below ##
+    return NotImplemented
+def addTwoListOfNumpy(list_of_numpy1, list_of_numpy2):
+    ## See below ##
+    return NotImplemented
+def avoidZeroValue(numpy_ndarray):
+    ## See below ##
+    return NotImplemented
+def printEpochAndLoss(model, data_x_part, data_y_part, epoch):
+    ## See below ##
+    return NotImplemented
+def printImage(enable_print_y, data_x_part, data_y_part=None):
+    ## See below ##
+    return NotImplemented
+def generatePkFromSource():
+    ## See below ##
+    return NotImplemented
+
+
+
+########## Implementation of all functions #####################################
+def initWeights(network_struct):
     numpy.random.seed()
-    weight_list, bias = [], []
     
-    # input layer to 1st hidden layer (W[0]), hidden layer to hidden layer, and hidden layer to output layer
-    for i in range(len(network_struct)-1):
-        # Skill from ch03 slide P.42 (pdf P.21)
-        weight_list.append( numpy.random.randn(network_struct[i], network_struct[i+1]) / numpy.sqrt(2.0*network_struct[0]) )
-        #b.append( numpy.zeros((network_struct[i+1], 1)) )
+    # Skill from ch03 slide P.42 (pdf P.21)
+    weight_list = [numpy.random.randn(network_struct[i], network_struct[i+1]) / numpy.sqrt(network_struct[0]) 
+                   for i in range(len(network_struct)-1)]
     
     return weight_list
 
@@ -101,7 +158,7 @@ def readImageLabel(file_path, file_type):
         elif file_type == "label":
             magic_num, label_num = struct.unpack(">II", fptr.read(8))
             result = numpy.frombuffer(fptr.read(), dtype=numpy.dtype(numpy.uint8))
-            result = oneHotVector(result)
+            result = oneHotEncoding(result)
         else:
             print("Error type of files.")
             result = -1
@@ -109,8 +166,6 @@ def readImageLabel(file_path, file_type):
     return result
 
 
-# Return a list of list of batches [batch_0, batch_1, batch_2,...], 
-# where batch_i = [(one_instance_x, one_instance_y), (x, y),...]
 def makeDataBatches(batch_size, data_x_part, data_y_part):
     index_list, start = [], 0
     
@@ -127,27 +182,30 @@ def makeDataBatches(batch_size, data_x_part, data_y_part):
         list_of_list.append(batch_i)
     
     return list_of_list
-    
 
-def oneHotVector(y_part):
-    one_hot = numpy.arange(10)
-    flag = True # First iteration of for loop
+
+def oneHotEncoding(data_y_part):
+    result = []
     
-    for label in y_part:
-        one_hot_label = (one_hot == int(label)).astype(numpy.float).reshape(10, 1)
-        if flag == True: 
-            result, flag = one_hot_label, False
-        else:
-            result = numpy.append(result, one_hot_label, axis=1)
+    for label in data_y_part:
+        temp = [0.0] * 10
+        temp[label] = 1.0
+        result.append(temp)
+    
+    result = numpy.asarray(result).transpose()
     
     # Avoid zeroes and ones which may cause gradient vanishing/exploding
-    result[result==0.0] = 0.001
-    result[result==1.0] = 0.999
+    #result[result==0.0] = 0.001
+    #result[result==1.0] = 0.999
     
     return result
 
 
 def trainModel(num_epochs, model, train_x_part, train_y_part, valid_x_part, valid_y_part):
+    pool = Pool() # Create multiprocessing pool
+    pool.close()
+    #skipgram_count = [row for row in pool.map(mapper, baby1t)]
+    
     # STEP1: Split training data into many mini-batches
     batch_list = makeDataBatches(CONST.batch_size(), train_x_part, train_y_part)
     printEpochAndLoss(model, valid_x_part, valid_y_part, 0.1)
@@ -176,7 +234,7 @@ def trainModel(num_epochs, model, train_x_part, train_y_part, valid_x_part, vali
                 accum_nabla_W = addTwoListOfNumpy(accum_nabla_W, one_instance_nabla_W)
             
             # Update model weights at a single time
-            model.W = updateModelWeight("mini-batch", model, accum_nabla_W, len(one_batch))
+            model.W = updateModelWeight("whole", model, accum_nabla_W, len(one_batch))
         
         # Calculate average C-E loss on validation set
         printEpochAndLoss(model, valid_x_part, valid_y_part, epoch+1)
@@ -207,40 +265,39 @@ def updateModelWeight(update_method, model, nabla_W, batch_size=0):
 
 
 def forwardPropagate(model, data_x_part):
-    # 'h' is a list of dicts which stores intermediate values (i.e., pre_activation_value and
-    #  post_activation_value) at hidden layers and output layer.
-    # NOTE: h[0]['post_act'] = input data x part
-    h = [{'pre_act': None, 'post_act': data_x_part}]
+    h = [{'pre_act': data_x_part, 'post_act': data_x_part}]
     
     for layer_index in range(len(model.network_struct)-1):
         if layer_index in list(range(len(model.network_struct)-2)): # hidden layer
-            pre_act  = calcPreActive(model.W[layer_index], h[-1]['post_act'])
+            pre_act  = calcPreActZ(model.W[layer_index], h[-1]['post_act'])
             post_act = actFunc(pre_act, "hidden") # The activation function is ReLU
             h.append({'pre_act': pre_act, 'post_act': post_act})
         else: # output layer
-            pre_act  = calcPreActive(model.W[layer_index], h[-1]['post_act'])
+            pre_act  = calcPreActZ(model.W[layer_index], h[-1]['post_act'])
             post_act = actFunc(pre_act, "output") # The activation function is softmax, not cross-entropy
             h.append({'pre_act': pre_act, 'post_act': post_act})
     return h
 
 
-# Return 'nabla_W'
-# 'nabla_W' is a layer-by-layer python-list of numpy arrays, similar to 'model.W'
-def backwardPropagate(model, FP_intermediates, truth_y):
+def backwardPropagate(model, Intermediates, truth_y):
+    # Prepare 'Z' and 'A' for convenient notation which would be used later.
+    Z, A = {}, {}
+    for i in range(len(Intermediates)):
+        Z[i-1] = Intermediates[i]['pre_act']
+        A[i-1] = Intermediates[i]['post_act']
+    
     nabla_W = [numpy.zeros(each_W.shape) for each_W in model.W] # Initialization
     
     # BP for output layer
-    delta_temp = deActFunc("output", predict_y=FP_intermediates[-1]['post_act'], truth_y=truth_y) # ${\delta}^{L}$
-    nabla_W[-1] = FP_intermediates[-2]['post_act'].dot(delta_temp.T) # $a^{L-1} {\delta}^{L}$
+    L = len(model.W)-1 # 'L' is the index of the output layer in the python-list.
+    delta_temp = deActFunc("output", predict_y=A[L], truth_y=truth_y) # ${\delta}^{L}$
+    nabla_W[L] = A[L-1].dot(delta_temp.T) # $a^{L-1} {\delta}^{L}$
     
     # BP for hidden layers
     for layer_index in reversed(range(len(model.network_struct)-2)):
-        # z^{l}   = FP_intermediates[layer_index+1]['pre_act']
-        # w^{l+1} = model.W[layer_index+1]
-        # a^{l-1} = FP_intermediates[layer_index]['post_act']
-        sigma_prime = deActFunc("hidden", z_vector=FP_intermediates[layer_index+1]['pre_act'])
-        delta_temp = (model.W[layer_index+1]).dot(delta_temp) * sigma_prime
-        nabla_W[layer_index] = FP_intermediates[layer_index]['post_act'].dot(delta_temp.T)
+        sigma_prime          = deActFunc("hidden", z_vector=Z[layer_index])
+        delta_temp           = (model.W[layer_index+1]).dot(delta_temp) * sigma_prime
+        nabla_W[layer_index] = A[layer_index-1].dot(delta_temp.T)
     
     return nabla_W
 
@@ -276,16 +333,16 @@ def deActFunc(layer_type, z_vector=None, predict_y=None, truth_y=None):
         exit()
     else:
         if layer_type == "hidden":
-            return FUNC.unitStep(z_vector)
+            return FUNC.dReLU(z_vector)
         else: # layer_type == "output"
-            if predict_y.shape != truth_y.shape:
+            if predict_y.shape == truth_y.shape:
+                return predict_y - truth_y # The derivative of function which is combined softmax and C-E loss
+            else:
                 print("Error: Calaculate derivative of output layer must use 'truth_y' with dim=(-1, 1).")
                 exit()
-            else:
-                return predict_y - truth_y # The derivative of function which is combined softmax and C-E loss
 
 
-def calcPreActive(weight, data_x_part):
+def calcPreActZ(weight, data_x_part):
     pre_act_value = (weight.T).dot(data_x_part)
     if len(pre_act_value.shape)==1: pre_act_value = pre_act_value.reshape(-1, 1)
     return pre_act_value
@@ -315,9 +372,9 @@ def addTwoListOfNumpy(list_of_numpy1, list_of_numpy2):
     return result
 
 
-def avoidZeroValue(numpy_vector):
-    # Add 0.01 to all elements of numpy.ndarray to avoid zeros which caused gradient vanishing
-    return numpy_vector + 0.0001
+def avoidZeroValue(numpy_ndarray):
+    # Add small amount to all elements of numpy.ndarray to avoid zeros which would cause gradient vanishing
+    return numpy_ndarray + 1e-8
 
 
 def printEpochAndLoss(model, data_x_part, data_y_part, epoch):
@@ -325,22 +382,17 @@ def printEpochAndLoss(model, data_x_part, data_y_part, epoch):
     predict_y_s = inference(model, data_x_part)
     for i in range(predict_y_s.shape[1]):
         ce_loss += FUNC.crossEntropy(predict_y_s[:, i], data_y_part[:, i])
-    print("Epoch=%d, Total loss=%.2f"%(int(epoch), ce_loss/predict_y_s.shape[1]/epoch))
+    print("Epoch=%d, Total loss=%.2f"%(int(epoch), ce_loss))
 
 
-def printImage(data_x_part, data_y_part):
+def printImage(enable_print_y, data_x_part, data_y_part=None):
     # Print data into a gray image
     pyplot.imshow(data_x_part.reshape(28, 28), cmap=pyplot.cm.gray)
-    pyplot.title("%s"%str(data_y_part))
+    if enable_print_y == True: pyplot.title("%s"%str(data_y_part))
     pyplot.show()
 
 
-# train_x_part : (28*28, 60000) i.e., (784, 60000)
-# train_y_part : (10, 60000)
-if __name__ == "__main__":
-    warnings.simplefilter("always")
-    start_time = time.time()
-    
+def generatePkFromSource():
     print("Open source files.")
     # Prepare absolute paths of input files
     working_dir           = pathlib.Path().absolute()
@@ -353,6 +405,28 @@ if __name__ == "__main__":
     # Load data from files
     train_x_part, train_y_part = loadData(train_image_file_path, train_label_file_path)
     test_x_part, test_y_part   = loadData(test_image_file_path, test_label_file_path)
+    
+    print("Store data from memory into .pickle file.")
+    with open('train-images.pickle', 'wb') as fp: pickle.dump(train_x_part, fp)
+    with open('train-labels.pickle', 'wb') as fp: pickle.dump(train_y_part, fp)
+    with open('test-images.pickle', 'wb') as fp: pickle.dump(test_x_part, fp)
+    with open('test-labels.pickle', 'wb') as fp: pickle.dump(test_y_part, fp)
+    exit()
+
+
+
+########## Main function #######################################################
+if __name__ == "__main__":
+    warnings.simplefilter("always")
+    start_time = time.time()
+    
+    #generatePkFromSource()
+    
+    print("Load data from .pickle.")
+    with open('train-images.pickle', 'rb') as fp: train_x_part = pickle.load(fp)
+    with open('train-labels.pickle', 'rb') as fp: train_y_part = pickle.load(fp)
+    with open('test-images.pickle', 'rb') as fp: test_x_part = pickle.load(fp)
+    with open('test-labels.pickle', 'rb') as fp: test_y_part = pickle.load(fp)
     
     print("Shuffle data.")
     # Shuffle data
@@ -370,7 +444,7 @@ if __name__ == "__main__":
     train_x_part_P70 = train_x_part[:, valid_num:]
     train_y_part_P30 = train_y_part[:, :valid_num]
     train_y_part_P70 = train_y_part[:, valid_num:]
-        
+    
     '''
     t_sne = manifold.TSNE(n_components=2, random_state=CONST.row_num())
     print("%%%%")
@@ -395,7 +469,7 @@ if __name__ == "__main__":
         pyplot.show()
     '''
     
-    nn_struct = [CONST.row_num()*CONST.col_num(), 100, 100, CONST.output_dim()]
+    nn_struct = [CONST.input_dim(), 7, 3, CONST.output_dim()]
     HW1_NN    = MODEL(network_struct=nn_struct, learning_rate=0.1)
     
     print("Start training a model.\n")
@@ -404,7 +478,8 @@ if __name__ == "__main__":
     
     print("\nPredict testing data set.\n")
     predictions = inference(HW1_NN, test_x_part)
-    
+        
+    '''
     print("[Result]")
     temp = 0
     for i in range(predictions.shape[1]):
@@ -414,11 +489,15 @@ if __name__ == "__main__":
             temp = temp
     print("Accuracy on test data: %.4f%%"%((1.0-temp/predictions.shape[1])*100.0))
     print("Total Exe. Seconds: %.2f"%(time.time()-start_time))
-    
+    '''
 
+
+########## Other Notes #########################################################
 # The # of nodes in the input layer is determined by the dimensionality of training data. => input layer 784 neurons
 # The # of nodes in the output layer is determined by the number of classes we have. => 10 classes 10 neurons 
 # Textbook P.139 : forward & backward function
 # `np_ay_1.dot(np_ay_2)` is equivalent to `numpy.dot(np_ay_1, np_ay_2)`
 # activations[layer_xx] i.e., FP_intermediates[layer_xx]['post_act']
 # zs[layer_xx] i.e., FP_intermediates[layer_xx]['pre_act']
+# train_x_part : (28*28, 60000) i.e., (784, 60000)
+# train_y_part : (10, 60000)
